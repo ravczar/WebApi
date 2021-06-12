bae62fd07a764f752e70fd3df5a12c561e4c2094
ravczar <ravczar@gmail.com>
2021-06-12 21:41:23 +0200

    Add Contracts and Logger Projects.

---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contracts;
+using NLog;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
