8ed4f253d8e3bbb7ab1fb78e5fe3b08ce3e9b7d6
ravczar <ravczar@gmail.com>
2021-06-19 15:38:31 +0200

    Add repository wrapper and its interface.

---
+++ Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Contracts
+{
+    /// <summary>
+    /// If controller needs data about may models he would have to instantiate all User/account repositories.
+    /// To ger limit instantiation of repos, create this RepoWrapper, that collects all models repo interfaces and includes Save method.
+    /// </summary>
+    public interface IRepositoryWrapper
+    {
+        IUserRepository User { get; set; }
+        IAccountRepository Account { get; set; }
+        void Save();
+    }
+}
---
+++ Repository/RepositoryWrapper.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        public IUserRepository User { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IAccountRepository Account { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
