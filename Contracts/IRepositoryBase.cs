1488478b5d2a8d83ad565f135e9b6010d27be15c
ravczar <ravczar@gmail.com>
2021-06-19 15:09:02 +0200

    Add comment to IrepositoryBase

--- Contracts/IRepositoryBase.cs
+++ Contracts/IRepositoryBase.cs
@@ -6,6 +6,14 @@ using System.Text;
 
 namespace Contracts
 {
+
+    /// <summary>
+    /// Generic repository that will serve us all the CRUD methods. 
+    /// As a result, all the methods can be called upon any repository class in our project.
+    /// This is a wrapper around repository class and we will inject is in Controllers as a service.
+    /// I will instantiate this only once and call it by any repository class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IRepositoryBase<T>
     {
         IQueryable<T> FindAll();
