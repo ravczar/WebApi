605713952af46c926ad2322835d1d4b950bae00c
ravczar <ravczar@gmail.com>
2021-06-19 15:14:42 +0200

    Add RepositoryContext property, and constructor in Repository base.

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -10,6 +10,13 @@ namespace Repository
 {
     public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        protected RepositoryContext RepositoryContext { get; set; }
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            this.RepositoryContext = repositoryContext;
+        }
+
         public void Create(T entity)
         {
             throw new NotImplementedException();
